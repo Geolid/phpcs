c10e4a7f5e2791c90c25869f744e87ec8ffaed72
alexandre fontfreyde <a.fontfreyde@geolid.com>
2018-02-06 17:51:01 +0100

    ADD: rules for naming conventions of interfaces, abstracts, traits and exceptions

---
+++ tests/Example/Naming/AbstractNaming.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Example\Naming;
+
+abstract class AbstractNaming
+{
+    public function superMethod()
+    {
+    }
+}
---
+++ tests/Example/Naming/NamingException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Example\Naming;
+
+class NamingException extends \Exception
+{
+
+}
